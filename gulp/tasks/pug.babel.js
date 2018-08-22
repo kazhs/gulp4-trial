4e8dc9ed1bfe632106fcc635424db53a602998ba
kz <kaz.18.orange@gmail.com>
2018-08-22 21:36:06 +0900

    Added local variables to pug

--- gulp/tasks/pug.babel.js
+++ gulp/tasks/pug.babel.js
@@ -3,7 +3,20 @@ import pug from 'gulp-pug';
 
 import { root } from './../../helper.babel';
 
+class PugPath {
+  constructor(_context = '') {
+    this._context = _context;
+  }
+
+  script(filename) { return `${this._context}/assets/script/${filename}.js`; }
+  style(filename) { return `${this._context}/assets/style/${filename}.css` }
+}
+
 export const transpilePug = () =>
   src(root('src', 'template', '**', '!(_)*.pug'))
-    .pipe(pug())
+    .pipe(pug({
+      locals: {
+        path: new PugPath()
+      }
+    }))
     .pipe(dest(root('public')));
